cf01c26a241d6cb82a609f11d5c29b8248304da1
brillk <74028699+brillk@users.noreply.github.com>
2021-10-21 10:50:31 +0900

    TextRPG4.final
    3명 중에 하나를 고르고 죽을 때 까지 싸우게 하는 프로그램이다.
    Pick 1 class from 3 classes and fight till they die.

--- CPP_Study/Game.cpp
+++ CPP_Study/Game.cpp
@@ -1,2 +1,58 @@
 #include "Game.h"
+#include <iostream>
+using namespace std;
+#include "Player.h"
+#include "Field.h"
 
+Game::Game() : _player(nullptr), _field(nullptr) {
+
+}
+
+Game::~Game() {
+	if (_player != nullptr)
+		delete _player;
+	if (_field != nullptr)
+		delete _field;
+}
+
+void Game::Init() {
+	_field = new Field();
+}
+
+void Game::Update() {
+
+	if(_player == nullptr)
+		CreatePlayer();
+
+	if (_player->IsDead())
+	{
+		delete _player;
+		_player = nullptr;
+		CreatePlayer();
+	}
+	_field->Update(_player);
+}
+
+void Game::CreatePlayer() {
+	while (_player == nullptr) {
+
+		cout << "---------" << endl;
+		cout << "캐릭 생성" << endl;
+		cout << "1) 기사 2) 궁수 3)법사" << endl;
+
+		cout << "> ";
+
+		int input = 0;
+		cin >> input;
+
+		if (input == PT_Knight) {
+			_player = new Knight();
+		}
+		else if (input == PT_Archer) {
+			_player = new Archer();
+		}
+		else if (input == PT_Mage) {
+			_player = new Mage();
+		}
+	}
+}
--- CPP_Study/Game.h
+++ CPP_Study/Game.h
@@ -1,9 +1,25 @@
 #pragma once
+#include "Player.h"
 
 
+//is - a / has - a 
+
+//전방선언
+class Player;
+class Field;
 
 class Game
 {
+public:
+	Game();
+	~Game();
 
+	void Init();
+	void Update();
+
+	void CreatePlayer();
+private:
+	Player* _player;
+	Field* _field;
 };
 
