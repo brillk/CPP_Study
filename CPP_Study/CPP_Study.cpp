c237429a830b4e5f7ce1b6d545c51766cb0e3b3a
brillk <74028699+brillk@users.noreply.github.com>
2021-10-25 17:30:47 +0900

    vector_final

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -2,74 +2,171 @@
 using namespace std;
 #include <vector>
 
-class MyIterator {
+template<typename T>
+class Iterator {
+public:
+	Iterator() : _ptr(nullptr) {
+
+	}
+
+	Iterator(T* ptr) : _ptr(ptr) {
+
+	}
+
+	Iterator& operator++() {
+		
+		_ptr++;
+		return *this;
+	}
+
+	Iterator& operator--() {
+
+		_ptr--;
+		return *this;
+	}
+
+	Iterator operator--(int) {
+		Iterator temp = *this;
+		_ptr++;
+		return temp; // 원본은 해치지 않고 복사한 값을 뱉기
+	}
+
+	Iterator operator++(int) {
+		Iterator temp = *this;
+		_ptr++;
+		return temp; // 원본은 해치지 않고 복사한 값을 뱉기
+	}
+
+	Iterator operator+(const int count) {
+		Iterator temp = *this;
+		temp._ptr += count;
+		return temp;
+	}
+
+
+	Iterator operator-(const int count) {
+		Iterator temp = *this;
+		temp._ptr -= count;
+		return temp;
+	}
+
+
+	bool operator==(const Iterator& right) {
+		return _ptr == right._ptr;
+	}
+
+	bool operator!=(const Iterator& right) {
+		return _ptr != right._ptr;
+	}
+
+	T& operator*() {
+		return *_ptr;
+	}
+
+public:
+
+	T* _ptr;
 
 };
 
-class MyVector {
+template <typename T>
+class Vector {
 public:
-	typedef MyIterator iterator;
+	Vector() : _data(nullptr), _size(0), _capacity(0) {
+
+	}
+
+	~Vector() {
+		if (_data)
+			delete[] _data;
+	}
+
+	
+	//
+	void push_back(const T& val) {
+		if ( _size == _capacity) {
+
+			//증설 작업
+			int newCapacity = static_cast<int>(_capacity * 1.5);
+			
+			if (newCapacity == _capacity)
+				newCapacity++;
+
+			reserve(newCapacity);
+		}
+		//size = 2
+		//[1][2] 2번 자리에 넣기
+		//데이터 저장
+		_data[_size] = val;
+
+		//데이터 개수 증가
+		_size++;
+	}
+
+
+
+	void reserve(int capacity) {
+		_capacity = capacity;
+
+		T* newData = new T[_capacity];
+
+		//데이터 복사
+		for (int i = 0; i < _size; i++) 
+			newData[i] = _data[i];
+		
+
+		//기존에 있던 데이터를 날린다
+		if (_data)
+			delete[] _data;
+
+		//교체 
+		_data = newData;
+
+	}
+
+	T& operator[](const int pos) { return _data[pos]; }
+
+	int size() { return _size; }
+	int capacity() { return _capacity; }
+
+public:
+	typedef Iterator<T> iterator;
+
+	//애만 다시 초기화 해주면 다시 시작하는 느낌으로 처음부터 시작한다
+	//push_back을 여기서 이어한다
+	void clear() { _size = 0; }
+	iterator begin() { return iterator(&_data[0]); }
+	iterator end() { return begin() + _size; }
+
+private:
+	T* _data;
+	int _size;
+	int _capacity;
 
 };
 
 int main()
-{	
+{
+	vector<int> v;
 	
-	
-	vector<int> v(10);
+	v.reserve(100); // 현재 가용 용량을 늘린다
 
-	for (vector<int>::size_type i = 0; i < v.size(); i++) 
-		v[i] = i;
-	
-
-	vector<int>::iterator itBegin = v.begin();
-	vector<int>::iterator itEnd = v.end();
-	
-	
-
-	//비교
-	int* ptrBegin = &v[0]; //v.begin()._Ptr;
-	int* ptrEnd = ptrBegin + 10; // v.end()._Ptr;
-
-	for (int* ptr = ptrBegin; ptr != ptrEnd; ++ptr) {
-		cout << (*ptr) << endl; // 결과 0~9
+	for (int i = 0; i < 100; i++) {
+		v.push_back(i);
+		cout << v.size() << " " << v.capacity() << endl;
 	}
 
-	/*
+	for (int i = 0; i < v.size(); i++) {
 
-	vector<- 동적 배열
-	1) vector의 동작원리 (size/capacity)
-	2) 중간 삽입/삭제 BAD
-	3) 처음/끝 삽입/삭제 BAD / GOOD
-	4) 임의 접근 (Random Access) 
-
-
-	vector = 원소가 하나의 메모리 블록에 연속하게 저장된다
-
-	[		] 
-	[1][2][3][4][5] - 중간에 값 바꾸기
-
-	*/
-
-	//vector<int>::iterator insertIt = v.insert(v.begin() + 2, 5);
-	//vector<int>::iterator eraseIt1 = v.erase(v.begin() + 2);
-	//vector<int>::iterator eraseIt2 = v.erase(v.begin() + 2, v.begin() + 4);
-
-	//스캔을 하면서, 3이라는 데이터가 있으면 일괄 삭제하고 싶을때
-	//
-
-	for (vector<int>::iterator it = v.begin(); it != v.end();) {
-		int data = *it;
-		if (data == 3) {
-
-			it = v.erase(it); // 크래시가 나는 이유	: 삭제가 실행되고 iterator가 가지는 데이터가 NULL이 되어 버려서 어떠한 형태로도 이용할수 없다 
-		 
-		}
-		else {
-			++it;
-		}
+		cout << v[i] << endl;
 	}
-	
+
+	cout << "--------------" << endl;
+
+	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+		cout << (*it) << endl;
+	}
+
 
 	return 0;
 
