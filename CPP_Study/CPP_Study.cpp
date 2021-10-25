b0713ab538268cfae2f1408ffcdf2645cf99c959
brillk <74028699+brillk@users.noreply.github.com>
2021-10-25 16:33:09 +0900

    vector3

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -14,54 +14,18 @@ public:
 
 int main()
 {	
-	/*
-	컨테이너 Container : 데이터를 저장하는 객체(자료구조 Data structure)
 	
-	vector<- 동적 배열
-	1) vector의 동작원리 (size/capacity) 
-	2) 중간 삽입/삭제
-	3)처음/끝 삽입/삭제
-	4) 임의 접근
-
-	*/
-
-
-	// 반복자(iterator) : 포인터와 유사한 개념, 컨테이너의 원소(데이터)를 가리키고, 다음 / 이전 원소로 이동가능
+	
 	vector<int> v(10);
 
 	for (vector<int>::size_type i = 0; i < v.size(); i++) 
 		v[i] = i;
 	
-	//vector<int>::iterator it; //나중에 나오는 리스트나 맵도 이런 iterator를 가지고 있다
-	//int* ptr;
 
-	//it = v.begin();
-	//ptr = &v[0]; //처음 주소를 ptr에 저장
-
-	//cout << (*it) << endl; // 결과 0
-	//cout << (*ptr) << endl; //결과 0
-
-	//메모리를 까서 it 와 ptr의 값을 확인했다
-	//어떤 컨테이너에 들어가 있는지 알수 있다
-
-	//++ 이나 --를 사용해 데이터의 크기만큼 이동 -> int에서 int의 다음 배열로
-	//응용
-	//
 	vector<int>::iterator itBegin = v.begin();
 	vector<int>::iterator itEnd = v.end();
-	//메모리 까기
-	//itBegin은 제대로 된 값, itEnd는 이상한 값이 나온다
-	//유효하지 않는 범위를 가리키고 있다 fdfdfdfdfd
-
-
-	//iterator는 vector뿐 아니라, 다른 컨테이너에도 공통적으로 있는 개념
-	//STL 한정으로 통일성이 생겨서 다른 자료구조로 넘어가면 배우기가 편하다
-	//다른 컨테이너는 v[i]와 같은 인덱스 접근이 안될수도 있다
-	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) { //++it 가  it++보다 쪼금 더 성능에 좋다
-		
-		cout << (*it) << endl;
-
-	}
+	
+	
 
 	//비교
 	int* ptrBegin = &v[0]; //v.begin()._Ptr;
@@ -71,16 +35,41 @@ int main()
 		cout << (*ptr) << endl; // 결과 0~9
 	}
 
+	/*
 
-	//정방향 읽기 ->
-	//vector<int>::const_iterator cit1 = v.cbegin();//const int*
-	
+	vector<- 동적 배열
+	1) vector의 동작원리 (size/capacity)
+	2) 중간 삽입/삭제 BAD
+	3) 처음/끝 삽입/삭제 BAD / GOOD
+	4) 임의 접근 (Random Access) 
 
-	//역방향 읽기 <-
-	for (vector<int>::reverse_iterator it = v.rbegin(); it != v.rend(); ++it) {
 
-		cout << (*it) << endl;
+	vector = 원소가 하나의 메모리 블록에 연속하게 저장된다
+
+	[		] 
+	[1][2][3][4][5] - 중간에 값 바꾸기
+
+	*/
+
+	//vector<int>::iterator insertIt = v.insert(v.begin() + 2, 5);
+	//vector<int>::iterator eraseIt1 = v.erase(v.begin() + 2);
+	//vector<int>::iterator eraseIt2 = v.erase(v.begin() + 2, v.begin() + 4);
+
+	//스캔을 하면서, 3이라는 데이터가 있으면 일괄 삭제하고 싶을때
+	//
+
+	for (vector<int>::iterator it = v.begin(); it != v.end();) {
+		int data = *it;
+		if (data == 3) {
+
+			it = v.erase(it); // 크래시가 나는 이유	: 삭제가 실행되고 iterator가 가지는 데이터가 NULL이 되어 버려서 어떠한 형태로도 이용할수 없다 
+		 
+		}
+		else {
+			++it;
+		}
 	}
+	
 
 	return 0;
 
