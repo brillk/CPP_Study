e82a2b2c91b1664c9fc774268cfa4b058bfb894d
brillk <74028699+brillk@users.noreply.github.com>
2021-10-27 09:14:34 +0900

    algorithm_basic use

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,111 +1,145 @@
-﻿#include <iostream>
-using namespace std;
+﻿using namespace std;
 #include <map>
 #include <deque>
 #include <list>
 #include <vector>
 #include <set>
+#include <iostream>
 
+//알고리즘
+#include <algorithm>
 
 
 int main()
 {
-	//key = value 가 일치하는 map -> set
-	set<int> s;
 
-	//넣고
-	//빼고
-	//찾고
-	//순회하고
+	//자료구조 (데이터를 저장하는 구조)
+	//알고리즘 (데이터를 어떻게 사용할것인가?)
 
-	//넣고
-	s.insert(10);
-	s.insert(50);
-	s.insert(40);
-	s.insert(20);
-	s.insert(60);
-	s.insert(80);
-	s.insert(90);
+	/*
+	find
+	find_if
 
-	////뺴고
-	s.erase(40);
+	count
+	count_if
 
-	//찾기
-	set<int>::iterator findit = s.find(50);
-	if (findit == s.end()) {
-		cout << "못찾음" << endl;
-	}
-	else {
-		cout << "찾음" << endl;
-	}
+	all_of
+	any_of
+	none_of
+	for_each
 
-	//순회하고
-	for (set<int>::iterator it = s.begin(); it != s.end(); ++it) {
-		cout << (*it) << endl; //키 값은 데이터 값
-	}
+	remove
+	remove_if
 	
-	cout << "-----------" << endl;
+	*/
+
+	srand(static_cast<unsigned int>(time(nullptr)));
+
+	vector<int> v;
 
 
-	multimap<int, int> mm;
-
-	//넣고
-	mm.insert(make_pair(1, 100));
-	mm.insert(make_pair(1, 300));
-	mm.insert(make_pair(1, 500));
-	mm.insert(make_pair(2, 400));
-	mm.insert(make_pair(2, 200));
-
-	//mm[1] = 500; 안됨
-
-	//빼고
-	//unsigned int count = mm.erase(1); //1로 되있는 키 다 삭제
-
-
-	//찾고
-	/*mm.find(1);
-	multimap<int, int>::iterator itFind = mm.find(1);
-	if (itFind != mm.end())
-		mm.erase(itFind);*/
-
-	pair<multimap<int, int>::iterator, multimap<int, int>::iterator> itPair;
-	itPair = mm.equal_range(1);
-
-	multimap<int, int>::iterator itBegin = mm.lower_bound(1);
-	multimap<int, int>::iterator itEnd = mm.upper_bound(1);
-
-	for (multimap<int, int>::iterator it = itPair.first; it != itPair.second; ++it) {
-		cout << it->first << " " << it->second << " " << endl;
+	for (int i = 0; i < 100; i++) {
+		int num = rand() % 100;
+		v.push_back(num);
 	}
 
-	cout << "----------" << endl;
+	//Q1 number라는 숫자가 벡터에 체크하는 기능 (bool, 첫등장 iterator)
+	{
+		int number = 50;
+		bool found = false;
+		vector<int>::iterator it = v.end();
 
-	multiset<int>ms;
-
-	//넣고
-	ms.insert(100);
-	ms.insert(100);
-	ms.insert(300);
-	ms.insert(100);
-	ms.insert(200);
-	ms.insert(200);
-
-
-	//찾기
-	multiset<int>::iterator findit2 = ms.find(100);
-	pair<multiset<int>::iterator, multiset<int>::iterator> itPair2;
-	itPair2 = ms.equal_range(100);
-
-	for (multiset<int>::iterator it = itPair2.first; it != itPair2.second; ++it) {
-		cout << *it << endl;
+		//또는 auto
+		vector<int>::iterator itFind = find(v.begin(), v.end(), number);
+		if (itFind == v.end()) {
+			cout << "cant find" << endl;
+		}
+		else {
+			cout << "find" << endl;
+		}
 	}
 
-	multiset<int>::iterator itBegin2 = ms.lower_bound(100);
-	multiset<int>::iterator itEnd2 = ms.upper_bound(100);
+	//Q2 11로 나뉘는 숫자가 벡터에 있는지 체크하는 기능
+	{
+		bool found = false;
+		vector<int>::iterator it;
+		
+		struct CanDiveby11 {
+			bool operator()(int n) {
+				return n % 11 == 0;
+			}
+		};
+
+		//범위안에서 찾는데 조건을 달고 있다
+		//조건은 함수로 파줬지만 미래에 배울 람다식을 쓰면 더 짧아진다고 한다
+		vector<int>::iterator itFind = find_if(v.begin(), v.end(), CanDiveby11());
+		if (itFind == v.end()) {
+			cout << "cant find" << endl;
+		}
+		else {
+			cout << "find" << endl;
+		}
+
+	}
 
 
-	for (multiset<int>::iterator it = itBegin2; it != itEnd2; ++it) {
-		cout << *it << endl;
+	//Q3 홀수인 숫자의 갯수는 count
+	{
+		int count = 0;
+
+		struct IsOdd {
+			bool operator()(int n) {
+				return (n % 2) != 0;
+			}
+		};
+		int n = count_if(v.begin(), v.end(), IsOdd());
+
+
+		//모든 데이터가 홀수인가
+		all_of(v.begin(), v.end(), IsOdd());
+		//홀수인 데이터가 하나라도 있습니까
+		any_of(v.begin(), v.end(), IsOdd());
+		//모든 데이터가 홀수가 아닙니까
+		none_of(v.begin(), v.end(), IsOdd());
+	}
+
+	//Q4 벡터에 들어가 있는 모든 숫자들에 3을 곱하라
+	{
+
+		struct Multi {
+			void operator()(int& n){
+				n = n * 3;
+			}
+		};
+
+		for_each(v.begin(), v.end(), Multi());
+	}
+
+
+	//홀수인 데이터를 일괄 삭제
+	{
+		v.clear();
+
+		v.push_back(1);
+		v.push_back(4);
+		v.push_back(2);
+		v.push_back(5);
+		v.push_back(3);
+
+		//remove(v.begin(), v.end(), 4);
+
+		struct IsOdd {
+			bool operator()(int n) {
+				return (n % 2) != 0;
+			}
+		};
+
+	
+		v.erase(remove_if(v.begin(), v.end(), IsOdd()));
+
+
+
+		int a = 3;
 	}
 
 	return 0;
