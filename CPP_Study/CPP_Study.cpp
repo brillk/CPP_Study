4c53918af1b07c99c58bbf3febd6db0726810c96
brillk <74028699+brillk@users.noreply.github.com>
2021-10-27 11:19:36 +0900

    nullptr

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -1,42 +1,88 @@
-﻿	using namespace std;
-	#include <map>
-	#include <deque>
-	#include <list>
-	#include <vector>
-	#include <set>
-	#include <iostream>
+﻿using namespace std;
+#include <map>
+#include <deque>
+#include <list>
+#include <vector>
+#include <set>
+#include <iostream>
 
 
 
-	class Knight {
-	public:
+class Knight {
+public:
+	void Test() {
 
-	};
+	}
+};
 
-	//중괄호 초기화 { }
-
-	int main()
-	{
-		//중괄호 초기화{}
-		int a = 0;
-		int b = { 0 };
+Knight* FindKnight() {
+	return nullptr;
+}
 
 
-		Knight k1;
-		Knight k2 = k1; //복사 연산자
+//중괄호 초기화 { }
 
-		Knight k3{ k1 }; 
+void Test(int a) {
+	cout << "test(int)" << endl;
+}
+	
+void Test(void* ptr) {		
+	cout << "test(*)" << endl;
+}
 
-		vector<int> v2{ 1,2,3,4 };
 
-		//장점 
-		//1) vector 등 컨테이너와 잘어울린다 초기화 문법의 일치화
+//NULLPTR 구현
 
-		//2) 축소 변환 방지
-		int x = 0;
-		double y{ x }; //오류
+const
+class {
+public:
 
-		//vector 등 특이한 케이스만 {} 사용
-
+	//어떤 타입의 포인터든 치환 가능
+	template<typename T>
+	operator T* () const {
 		return 0;
-	};
+	}
+
+	//어떤 타입의 멤버 포인터든 치환 가능
+	template<typename C, typename T>
+	operator T C::* () const {
+		return 0;
+	}
+
+private:
+	void operator&() const;//주소값 &을 막는다
+} _NullPtr;
+
+
+
+int main()
+{
+	int* ptr = NULL; //0, null 같음
+
+	//오동작을 위해 필요함
+	Test(0);
+	Test(NULL); 
+	Test(_NullPtr);
+
+
+	//가독성이 좋다
+	auto Knight = FindKnight();
+	if (Knight == _NullPtr) {
+
+	}
+
+	void (Knight:: * memFunc)();
+	memFunc = &Knight::Test;
+
+	if (memFunc == _NullPtr) {
+
+	}
+
+
+
+	//nullptr_t whoami = nullptr;
+
+
+
+	return 0;
+};
