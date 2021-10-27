a6e22a076e5bdeb5bf04a784ce69397876a4202f
brillk <74028699+brillk@users.noreply.github.com>
2021-10-27 13:34:35 +0900

    override & final?

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -7,45 +7,38 @@
 #include <iostream>
 
 
-class Knight {
+class Player {
 public:
-
-public:
-	//정의되지 않은 비공개(private) 함수	
-	void operator=(const Knight& k) = delete; //외부에선 복사가 안되지만, 내부에선 가능
-
-	//뚫어주는 예외
-	//friend class Admin;
-	//private vs friend 
-
-private:
-	int _hp = 100;
-
-};
-
-class Admin {
-public:
-	void Copy(const Knight& k) {
-		Knight k1;
-
-		//복사 연산	
-		k1 = k;
+	virtual void Attack() {
+		cout << "Player" << endl;
 	}
 };
 
+class Knight : public Player {
+public:
+
+	//재정의 override
+	virtual void Attack() override //attack을 부모에게 물려받았다
+	{
+		
+		cout << "Knight" << endl;
+	}
+public:
+	
+};
+
+
 int main()
 {
+
 	
-	Knight k1;
-	Knight k2;
+	Player* player = new Knight();
 
-	//복사 연산자
-	//k1 = k2;
-
-
-	Admin admin;
-	admin.Copy(k1);
+	player->Attack();
 	return 0;
+
+	//부모가 누군지 자식 클래스가 뭔지 알기 어렵다
+
 
 	
 };
