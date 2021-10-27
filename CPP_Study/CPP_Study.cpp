fab24574a3c407de85e35fcb466e819a799f5c06
brillk <74028699+brillk@users.noreply.github.com>
2021-10-27 11:27:56 +0900

    using > typedef

--- CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study.cpp
@@ -7,82 +7,31 @@
 #include <iostream>
 
 
+//using
 
-class Knight {
-public:
-	void Test() {
-
-	}
-};
-
-Knight* FindKnight() {
-	return nullptr;
-}
+typedef  __int64 id;
+using id2 = int;	//이름이 먼저?
 
 
-//중괄호 초기화 { }
+//1) 직관성
+typedef void (*MyFunc)(); //typedef < using
+using MyFunc2 = void(*)(); //가독성
 
-void Test(int a) {
-	cout << "test(int)" << endl;
-}
-	
-void Test(void* ptr) {		
-	cout << "test(*)" << endl;
-}
+//2) 템플릿
 
+template<typename T>
+using List = std::vector<T>;
 
-//NULLPTR 구현
-
-const
-class {
-public:
-
-	//어떤 타입의 포인터든 치환 가능
-	template<typename T>
-	operator T* () const {
-		return 0;
-	}
-
-	//어떤 타입의 멤버 포인터든 치환 가능
-	template<typename C, typename T>
-	operator T C::* () const {
-		return 0;
-	}
-
-private:
-	void operator&() const;//주소값 &을 막는다
-} _NullPtr;
-
-
+//결론 : using을 쓰자
 
 int main()
 {
-	int* ptr = NULL; //0, null 같음
+	id playerid = 0;
 
-	//오동작을 위해 필요함
-	Test(0);
-	Test(NULL); 
-	Test(_NullPtr);
-
-
-	//가독성이 좋다
-	auto Knight = FindKnight();
-	if (Knight == _NullPtr) {
-
-	}
-
-	void (Knight:: * memFunc)();
-	memFunc = &Knight::Test;
-
-	if (memFunc == _NullPtr) {
-
-	}
-
-
-
-	//nullptr_t whoami = nullptr;
-
-
+	List<int> li;
+	li.push_back(1);
+	li.push_back(2);
+	li.push_back(3);
 
 	return 0;
 };
